8a9d70367eb6a5f75f48fab316757286fe8e61f1
Yamato <aizawayamato@aizawa-no-MacBook-Air.local>
2018-12-17 00:49:36 +0900

    implements user authentication features

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # BoardsControllerクラスのindexメソッドを実行する
   root 'home#index'
-  resuorces :users, only: %i[new create]
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
